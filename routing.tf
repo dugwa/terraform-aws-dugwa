1d5b9ebf9403819b0608437f9737999a614b2e26
Daniel Ugwa <daniel.ugwa@skybettingandgaming.com>
2017-07-08 21:31:11 +0100

    remove unused private subnet to save aws money on resources

--- routing.tf
+++ routing.tf
@@ -40,21 +40,3 @@ resource "aws_route_table" "public" {
         gateway_id = "${aws_internet_gateway.gw.id}"
     }
 }
-resource "aws_route_table" "private" {
-  vpc_id = "${aws_vpc.london_paddington.id}"
-  tags {
-      Name = "Private"
-  }
-  route {
-        cidr_block = "0.0.0.0/0"
-        nat_gateway_id = "${aws_nat_gateway.Public.id}"
-  }
-}
-resource "aws_eip" "forNat" {
-    vpc      = true
-}
-resource "aws_nat_gateway" "Public" {
-    allocation_id = "${aws_eip.forNat.id}"
-    subnet_id = "${aws_subnet.Public.id}"
-    depends_on = ["aws_internet_gateway.gw"]
-}
--- subnet.tf
+++ subnet.tf
@@ -10,15 +10,3 @@ resource "aws_route_table_association" "Public" {
     subnet_id = "${aws_subnet.Public.id}"
     route_table_id = "${aws_route_table.public.id}"
 }
-resource "aws_subnet" "Private" {
-  vpc_id = "${aws_vpc.london_paddington.id}"
-  cidr_block = "${var.Subnet-Private}"
-  tags {
-        Name = "Private"
-  }
-  availability_zone = "${data.aws_availability_zones.available.names[1]}"
-}
-resource "aws_route_table_association" "Private" {
-    subnet_id = "${aws_subnet.Private.id}"
-    route_table_id = "${aws_route_table.private.id}"
-}
